83544d09d8b6dfcaa7382f552fb352ded0d3f138
Serhiy Vasylchuk <s.vasylchuk@betinvest.com>
2019-07-18 18:49:03 +0300

    added new functionality

--- src/game_server_app.erl
+++ src/game_server_app.erl
@@ -4,6 +4,8 @@
 -module(game_server_app).
 
 -behaviour(application).
+-include_lib("kernel/include/logger.hrl").
+-include_lib("game_server_int.hrl").
 
 %% Application callbacks
 -export([start/2, stop/1]).
@@ -12,9 +14,14 @@
 %%====================================================================
 -spec start(any(), any()) -> {ok, pid()}.
 start(_Type, _Args) ->
+    {ok, Val} = application:get_env(episcina, pools),
+    Opt = proplists:get_value(db1, Val),
+    {ok, _} = episcina:start_pool({?DB, Opt}),
+    ?LOG_INFO("init :", []),
+    io:format("asdasdasdasd"),
     Dispatch = cowboy_router:compile([
         {'_', [
-            {"/", game_server_handler, []}
+            {"/[...]", game_server_handler, []}
         ]}
     ]),
     {ok, _} = cowboy:start_clear(my_http_listener, [{port, 8080}], #{
--- src/game_server_handler.erl
+++ src/game_server_handler.erl
@@ -1,21 +1,39 @@
 %% @author Sergiy Vasylchuk
 -module(game_server_handler).
 
--behaviour(cowboy_http_handler).
+-behaviour(cowboy_handler).
 
--export([init/2, handle/2]).
+-export([init/2, handle/2, terminate/3]).
+-export([
+    allowed_methods/2,
+content_types_provided/2,
+    to_json/2,
+    from_json/2,
+    content_types_accepted/2
+]).
 
 -include_lib("kernel/include/logger.hrl").
 
 init(Req, State) ->
     ?LOG_INFO("init :~p", [Req]),
-    {ok, Req, State}.
+    io:format("zzz"),
+    {cowboy_rest, Req, State}.
 
 handle(Req, State) ->
     Method = cowboy_req:method(Req),
     HasBody = cowboy_req:has_body(Req),
+    io:format("zzasdasz"),
     {ok, Req3} = parse_req(Method, HasBody, Req),
     {ok, Req3, State}.
+
+allowed_methods(Req, State) ->
+    {[<<"POST">>], Req, State}.
+
+content_types_accepted(Req, State) ->
+    {[{<<"application/json">>}, from_json], Req, State}.
+
+content_types_provided(Req, State) ->
+    {[{{<<"application/json">>}, to_json}], Req, State}.
 
 %%parse_req(<<"OPTIONS">>, _HasBody, Req) ->
 %%    Req1 = cowboy_req:set_resp_header(
@@ -32,7 +50,7 @@ parse_req(<<"POST">>, true, Req) ->
     Action = maps:get(<<"action">>, Json),
     Res = parse_command(Action, Json),
     ?LOG_INFO("adasd :adsasdasdas", []),
-    cowboy_req:reply(200, [{<<"content-type">>, <<"application/json; charset=utf-8">>}], jsx:encode(Res), Req2);
+    cowboy_req:reply(200, [{<<"content-type">>, <<"application/json">>}], jsx:encode(Res), Req2);
 
 parse_req(<<"POST">>, false, Req) ->
     io:format("parse_req POST Missing body ~n",  []),
@@ -41,7 +59,7 @@ parse_req(<<"POST">>, false, Req) ->
 parse_req(_, _, Req) ->
     cowboy_req:reply(200, [{<<"content-type">>, <<"application/json; charset=utf-8">>}],  jsx:encode(#{<<"z">> => <<"zzzzazz">>}), Req).
 
-parse_command(<<"register">>, Json) ->
+parse_command(<<"/register">>, Json) ->
     Nickname = maps:get(<<"nickname">>, Json),
     game_server:register(Nickname);
 
@@ -49,4 +67,13 @@ parse_command(<<"authorize">>, Json) ->
     Uid = maps:get(<<"uid">>, Json),
     game_server:authorize(Uid).
 
+to_json(Req, State) ->
+    Body = jsx:encode(#{<<"status">> => <<"Success">>}),
+    {Body, Req, State}.
 
+from_json(Req, State) ->
+    Body = jsx:decode(<<"{\"status\" :\"Success\"}">>, [return_maps]),
+    {Body, Req, State}.
+
+terminate(_Reason, _Req, _State) ->
+    ok.
